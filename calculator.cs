39a7bab2f48f967d90e324eb1b734b1acbf4e651
abyanmusyaffa <abyanaufaalifmusyaffa@gmail.com>
2026-04-09 09:51:35 +0700

    menambahkan .cs

---
+++ calculator.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+
+        static double Pembagian(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
